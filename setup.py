077521a3c45bbdee5dd8892be38b617145cfb68e
Evgeniy Blinov <zheni-b@yandex.ru>
2023-09-15 22:46:25 +0300

    quotation marks

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as readme_file:
+with open('README.md', 'r') as readme_file:
     readme = readme_file.read()
 
 requirements = []
 
 setup(
-    name="awaits",
-    version="0.0.2",
-    author="Evgeniy Blinov",
-    author_email="zheni-b@yandex.ru",
-    description="async + threads + decorators = ?",
+    name='awaits',
+    version='0.0.2',
+    author='Evgeniy Blinov',
+    author_email='zheni-b@yandex.ru',
+    description='async + threads + decorators = ?',
     long_description=readme,
-    long_description_content_type="text/markdown",
-    url="https://github.com/pomponchik/awaits",
+    long_description_content_type='text/markdown',
+    url='https://github.com/pomponchik/awaits',
     packages=find_packages(),
     install_requires=requirements,
     classifiers=[
-        "Programming Language :: Python :: 3.8",
-        "License :: OSI Approved :: MIT License",
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: MIT License',
     ],
 )
