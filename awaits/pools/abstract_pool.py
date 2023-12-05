23b7e062f3de2e7cc783ab5d45c4715d59c35791
Evgeniy Blinov <zheni-b@yandex.ru>
2023-12-05 16:02:15 +0300

    abstract methods + the order of methods in the AbstractPool

--- awaits/pools/abstract_pool.py
+++ awaits/pools/abstract_pool.py
@@ -1,7 +1,9 @@
+from abc import ABC, abstractmethod
+
 from awaits.task import Task
 
 
-class AbstractPool:
+class AbstractPool(ABC):
     def __init__(self, pool_size):
         self.active = False
         self.pool_size = pool_size
@@ -9,6 +11,23 @@ class AbstractPool:
         self.workers = self.create_workers()
         self.activate_workers()
         self.active = True
+    
+    def __len__(self):
+        return self.pool_size
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}({len(self)})'
+
+    def __str__(self):
+        active = 'active' if self.active else 'not active'
+        return f'<{self.__class__.__name__} pool object of {len(self)} workers #{id(self)}, {active}>'
+
+    def __getitem__(self, index):
+        if not isinstance(index, int):
+            raise ValueError('Key must be an integer number.')
+        if index > len(self):
+            raise IndexError(f'Lenth of the pool is equal {len(self)}.')
+        return self.workers[index]
 
     def get_queue(self):
         if not hasattr(self, 'queue'):
@@ -29,37 +48,26 @@ class AbstractPool:
             workers.append(worker)
         return workers
 
-    def create_worker(self, index):  # pragma: no cover
-        raise NotImplementedError('The create workers operation is not defined.')
+    @abstractmethod
+    def create_worker(self, index):
+        ... # pragma: no cover
 
+    @abstractmethod
     def activate_workers(self, workers=None):  # pragma: no cover
-        raise NotImplementedError('The activate workers operation is not defined.')
+        ... # pragma: no cover
 
+    @abstractmethod
     def queue_size(self):  # pragma: no cover
-        raise NotImplementedError('The queue_size operation is not defined.')
+        ... # pragma: no cover
 
+    @abstractmethod
     def get_queue_class(self):  # pragma: no cover
-        raise NotImplementedError('The queue class is not defined.')
+        ... # pragma: no cover
 
+    @abstractmethod
     def put_to_queue(self, task):  # pragma: no cover
-        raise NotImplementedError('The put operation is not defined.')
+        ... # pragma: no cover
 
+    @abstractmethod
     def get_where_to_execute(self):  # pragma: no cover
-        raise NotImplementedError('The class to execute workers is not defined.')
-
-    def __len__(self):
-        return self.pool_size
-
-    def __repr__(self):
-        return f'{self.__class__.__name__}({len(self)})'
-
-    def __str__(self):
-        active = 'active' if self.active else 'not active'
-        return f'<{self.__class__.__name__} pool object of {len(self)} workers #{id(self)}, {active}>'
-
-    def __getitem__(self, index):
-        if not isinstance(index, int):
-            raise ValueError('Key must be an integer number.')
-        if index > len(self):
-            raise IndexError(f'Lenth of the pool is equal {len(self)}.')
-        return self.workers[index]
+        ... # pragma: no cover
