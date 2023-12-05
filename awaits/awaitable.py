a40b66742b8613bd9189b0de69e7526bc2cb44aa
Evgeniy Blinov <zheni-b@yandex.ru>
2023-12-05 19:06:03 +0300

    style fix

--- awaits/awaitable.py
+++ awaits/awaitable.py
@@ -8,16 +8,16 @@ from awaits.utils.end_of_wrappers import end_of_wrappers
 def awaitable(*args, pool=None, delay=None):
     pool = get_pool_for_decorator(pool)
 
-    def wrapper_of_wrapper(func):
-        @wraps(func)
+    def wrapper_of_wrapper(function):
+        @wraps(function)
         async def wrapper(*args, **kwargs):
             sleep_time = CommonData().delay if delay is None else delay
-            task = pool.do(func, *args, **kwargs)
+            task = pool.do(function, *args, **kwargs)
             while not task.done:
                 await sleep(sleep_time)
             if task.error:
                 raise task.exception
             return task.result
         return wrapper
-    
+
     return end_of_wrappers(args, wrapper_of_wrapper)
