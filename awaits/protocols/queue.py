34d1067afa5097b1fd4cc46a57510584e6cb989c
Evgeniy Blinov <zheni-b@yandex.ru>
2023-12-05 18:37:51 +0300

    protocol from typing_extensions

--- awaits/protocols/queue.py
+++ awaits/protocols/queue.py
@@ -1,4 +1,9 @@
-from typing import Protocol, Any
+from typing import Any
+
+try:
+    from typing import Protocol
+except ImportError:
+    from typing_extensions import Protocol
 
 
 class QueueProtocol(Protocol):
