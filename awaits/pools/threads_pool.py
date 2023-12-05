2b66c843c9365077aecddc32d66f1283440b6acb
Evgeniy Blinov <zheni-b@yandex.ru>
2023-12-05 19:13:04 +0300

    simplify it

--- awaits/pools/threads_pool.py
+++ awaits/pools/threads_pool.py
@@ -10,12 +10,8 @@ class ThreadsPool(AbstractPool):
     def get_queue(self) -> Queue:
         return Queue()
 
-    def get_worker_class(self):
-        return ThreadUnit
-
     def create_worker(self, index: int) -> AbstractUnit:
-        worker_class = self.get_worker_class()
-        worker = Thread(target=worker_class(self.queue, self, index).run)
+        worker = Thread(target=ThreadUnit(self.queue, self, index).run)
         worker.daemon = True
         worker.start()
         return worker
