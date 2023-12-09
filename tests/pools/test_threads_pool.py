fe84b372770b1aea6081a3d2a98557266b3e742a
Evgeniy Blinov <zheni-b@yandex.ru>
2023-12-09 13:13:47 +0300

    some tests

--- tests/pools/test_threads_pool.py
+++ tests/pools/test_threads_pool.py
@@ -6,3 +6,17 @@ from awaits.pools.threads_pool import ThreadsPool
 def test_create_pull_with_zero_workers():
     with pytest.raises(ValueError):
         ThreadsPool(0)
+
+
+def test_len_of_pool():
+    assert len(ThreadsPool(5)) == 5
+
+
+def test_repr_of_pool():
+    assert repr(ThreadsPool(5)) == 'ThreadsPool(5)'
+
+
+def test_str_of_pool():
+    pool = ThreadsPool(5)
+
+    assert str(pool) == f'<ThreadsPool pool object of 5 workers #{id(pool)}, active>'
