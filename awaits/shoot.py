b56639a9940c572f89836453a859147194f41a8c
Evgeniy Blinov <zheni-b@yandex.ru>
2024-01-05 16:41:37 +0300

    type hints

--- awaits/shoot.py
+++ awaits/shoot.py
@@ -1,5 +1,5 @@
 from functools import wraps
-from typing import Callable, Union, Optional, Any
+from typing import Awaitable, Callable, Union, Optional, Any
 
 from awaits.utils.get_pool_for_decorator import get_pool_for_decorator
 from awaits.utils.end_of_wrappers import end_of_wrappers
@@ -7,7 +7,7 @@ from awaits.pools.abstract_pool import AbstractPool
 from awaits.task import Task
 
 
-def shoot(*args: Callable[[Any], Any], pool: Optional[Union[str, AbstractPool]] = None):
+def shoot(*args: Callable[[Any], Any], pool: Optional[Union[str, AbstractPool]] = None) -> Union[Callable[[Callable[[Any], Any]], Callable[[Any], Awaitable[Any]]], Callable[[Any], Awaitable[Any]]]:
     pool = get_pool_for_decorator(pool)
 
     def wrapper_of_wrapper(function: Callable[[Any], Any]) -> Callable[[Any], Task]:
