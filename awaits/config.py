8d8f460da5e39f0a884f8e74b042694bc2a4a3a0
Evgeniy Blinov <zheni-b@yandex.ru>
2024-01-06 14:30:36 +0300

    empty lines

--- awaits/config.py
+++ awaits/config.py
@@ -15,16 +15,20 @@ class config:
     @staticmethod
     def set(**kwargs: Union[int, float]) -> None:
         new_kwargs = {}
+
         for key, value in kwargs.items():
             if key not in config.allowed_settings:
                 raise KeyError(f'"{key}" variable is not allowed for the awaits settings.')
             allowed_types = config.allowed_settings[key]
+
             is_allowed = False
             for one in allowed_types:
                 if isinstance(value, one):
                     is_allowed = True
+
             if not is_allowed:
                 allowed_types = ', '.join(allowed_types)
                 raise TypeError(f'Variable "{key}" has not one of types: {allowed_types}.')
             new_kwargs[key] = value
+        
         CommonData(**new_kwargs)
