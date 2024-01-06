0cd4e03036a21efd316440a41e3a6e6bdb881504
Evgeniy Blinov <zheni-b@yandex.ru>
2024-01-06 14:43:18 +0300

    +1 test

--- tests/test_config.py
+++ tests/test_config.py
@@ -9,3 +9,8 @@ def test_set_incompatible_typed_variable():
 
     with pytest.raises(TypeError, match='Variable "kek" has not one of types: int, float.'):
         config.set(delay='kek')
+
+
+def test_unknown_key_name():
+    with pytest.raises(KeyError, match='"kek" variable is not allowed for the awaits settings.'):
+        config.set(kek='kek')
