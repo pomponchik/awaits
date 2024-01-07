9792a6769a2b5d49154bf55263241605ff57bfa7
Evgeniy Blinov <zheni-b@yandex.ru>
2024-01-07 17:57:02 +0300

    explicit reexports

--- awaits/__init__.py
+++ awaits/__init__.py
@@ -1,3 +1,3 @@
-from awaits.awaitable import awaitable  # noqa: F401
-from awaits.shoot import shoot  # noqa: F401
-from awaits.config import config  # noqa: F401
+from awaits.awaitable import awaitable as awaitable  # noqa: F401
+from awaits.shoot import shoot as shoot  # noqa: F401
+from awaits.config import config as config  # noqa: F401
