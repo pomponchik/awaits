40229fdc986ecec39f764656bd1b0b69b1338daa
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2025-03-10 02:36:16 +0300

    fixes by lint issues

--- tests/test_task.py
+++ tests/test_task.py
@@ -74,5 +74,5 @@ def test_repr_task():
 
     task = Task(function, 1, 2, c="kek")
 
-    assert repr(task) == f'Task(function, 1, 2, c="kek")'
-    assert str(task) == f'Task(function, 1, 2, c="kek")'
+    assert repr(task) == 'Task(function, 1, 2, c="kek")'
+    assert str(task) == 'Task(function, 1, 2, c="kek")'
