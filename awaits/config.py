952eb42d0dd02f3565213ed7acd1dad8145fb366
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2025-12-15 21:45:32 +0300

    Add type: ignore[assignment] comments to config fields

--- awaits/config.py
+++ awaits/config.py
@@ -4,7 +4,7 @@ from skelet import Storage, Field, for_tool
 
 
 class Config(Storage, sources=for_tool('my_tool_name')):
-    pool_size: int = Field(10, validation=lambda x: x > 0)
-    delay: Union[int, float] = Field(0.001, validation=lambda x: x >= 0)
+    pool_size: int = Field(10, validation=lambda x: x > 0)  # type: ignore[assignment]
+    delay: Union[int, float] = Field(0.001, validation=lambda x: x >= 0)  # type: ignore[assignment]
 
 config = Config()
