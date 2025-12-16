315d23e27ca8678182bd900acb19b955af7d5dc0
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2025-12-16 03:43:15 +0300

    Update Config source to 'awaits'

--- awaits/config.py
+++ awaits/config.py
@@ -3,7 +3,7 @@ from typing import Union
 from skelet import Field, Storage, for_tool
 
 
-class Config(Storage, sources=for_tool('my_tool_name')):
+class Config(Storage, sources=for_tool('awaits')):
     pool_size: int = Field(10, validation=lambda x: x > 0)  # type: ignore[assignment]
     delay: Union[int, float] = Field(0.001, validation=lambda x: x >= 0)  # type: ignore[assignment]
 
