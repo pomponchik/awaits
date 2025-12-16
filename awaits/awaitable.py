899608d30d5cb31956bede67f5907f9fd29bfc9a
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2025-12-16 03:35:20 +0300

    Remove unused type ignore from awaitable signature

--- awaits/awaitable.py
+++ awaits/awaitable.py
@@ -11,7 +11,7 @@ from awaits.utils.get_pool_for_decorator import get_pool_for_decorator
 
 
 # TODO: delete the "type: ignore" if the minimal python version is > 3.9
-def awaitable(*args: Callable[FunctionParameters, FunctionResult], pool: Optional[Union[str, AbstractPool]] = None, delay: Optional[Union[int, float]] = None) -> Union[Callable[[Callable[FunctionParameters, FunctionResult]], Callable[FunctionParameters, Awaitable[FunctionResult]]], Callable[FunctionParameters, Awaitable[FunctionResult]], Callable[[Callable[FunctionParameters, FunctionResult]], Callable[FunctionParameters, Task]]]:  # type: ignore[valid-type]
+def awaitable(*args: Callable[FunctionParameters, FunctionResult], pool: Optional[Union[str, AbstractPool]] = None, delay: Optional[Union[int, float]] = None) -> Union[Callable[[Callable[FunctionParameters, FunctionResult]], Callable[FunctionParameters, Awaitable[FunctionResult]]], Callable[FunctionParameters, Awaitable[FunctionResult]], Callable[[Callable[FunctionParameters, FunctionResult]], Callable[FunctionParameters, Task]]]:  # type: ignore[valid-type, unused-ignore]
     pool = get_pool_for_decorator(pool)
 
     def wrapper_of_wrapper(function: Callable[FunctionParameters, FunctionResult]) -> Callable[FunctionParameters, Awaitable[FunctionResult]]:
