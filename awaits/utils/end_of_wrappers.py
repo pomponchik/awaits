020990eaee3d8cea410ab37b230e434141230a4a
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2025-12-16 03:18:27 +0300

    Some typing bullshit

--- awaits/utils/end_of_wrappers.py
+++ awaits/utils/end_of_wrappers.py
@@ -1,15 +1,19 @@
-from typing import Any, Callable, Tuple, Union
+from typing import Any, Callable, Tuple, Union, Awaitable
 
+from awaits.task import Task
 from awaits.errors import IncorrectUseOfTheDecoratorError
-from awaits.types import FunctionParameters
+from awaits.types import FunctionParameters, FunctionResult
 
 
-def end_of_wrappers(args: Tuple[Callable[FunctionParameters, Any], ...], wrapper: Callable[[Callable[FunctionParameters, Any]], Callable[FunctionParameters, Any]]) -> Union[Callable[[Callable[FunctionParameters, Any]], Callable[FunctionParameters, Any]], Callable[FunctionParameters, Any]]:
+
+
+
+def end_of_wrappers(args: Tuple[Callable[FunctionParameters, FunctionResult], ...], wrapper: Union[Callable[[Callable[FunctionParameters, FunctionResult]], Callable[FunctionParameters, Awaitable[FunctionResult]]], Callable[[Callable[FunctionParameters, FunctionResult]], Callable[FunctionParameters, Task]]]) -> Union[Callable[[Callable[FunctionParameters, FunctionResult]], Callable[FunctionParameters, Awaitable[FunctionResult]]], Callable[FunctionParameters, Awaitable[FunctionResult]], Callable[[Callable[FunctionParameters, FunctionResult]], Callable[FunctionParameters, Task]]]:
     """
     Определяем, как вызван декоратор - как фабрика декораторов (т. е. без позиционных аргументов) или как непосредственный декоратор.
     """
     if not len(args):
         return wrapper
     if len(args) == 1 and callable(args[0]):
-        return wrapper(args[0])
+        return wrapper(args[0])  # type: ignore[return-value]
     raise IncorrectUseOfTheDecoratorError('You used the awaitable decorator incorrectly. Read the documentation.')
