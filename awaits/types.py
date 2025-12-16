8e28383c54e871a0f2b403a7f53004c9bbe33222
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2025-12-16 03:27:30 +0300

    Add pragma to suppress coverage warning for fallback import

--- awaits/types.py
+++ awaits/types.py
@@ -4,7 +4,7 @@ from typing import TypeVar
 # TODO: Delete this try-except if python version is >= 3.10.
 try:
     from typing import ParamSpec  # type: ignore[attr-defined, unused-ignore]
-except ImportError:
+except ImportError:  # pragma: no cover
     from typing_extensions import ParamSpec  # type: ignore[assignment, unused-ignore]
 
 FunctionParameters = ParamSpec('FunctionParameters')
