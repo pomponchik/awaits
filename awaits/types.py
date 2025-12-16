adcac2f7d09c499c9457e95a39fa9132feb21240
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2025-12-16 03:23:42 +0300

    Add mute commment for mypy + TODO about deleting some python version related thing

--- awaits/types.py
+++ awaits/types.py
@@ -1,10 +1,11 @@
 # noqa: A005
 from typing import TypeVar
 
+# TODO: Delete this try-except if python version is >= 3.10.
 try:
     from typing import ParamSpec  # type: ignore[attr-defined]
 except ImportError:
-    from typing_extensions import ParamSpec  # type: ignore[assignment]
+    from typing_extensions import ParamSpec  # type: ignore[assignment, unused-ignore]
 
 FunctionParameters = ParamSpec('FunctionParameters')
 FunctionResult = TypeVar('FunctionResult')
